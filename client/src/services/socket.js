91c10f080aa02a0e5beaaae16c5a8c061b54de07
Abhijeet Singh <asingh9829@gmail.com>
2021-07-25 03:28:14 +0530

    updated peer host

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -2,4 +2,4 @@ import io from 'socket.io-client'
 // import {SOCKET_URL} from 'config'
 
 const options = {auth: {token: localStorage.authToken}}
-export const socket = io('localhost:3000', options)
+export const socket = io(process.env.REACT_APP_API_URL, options)
