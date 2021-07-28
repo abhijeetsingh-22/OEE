14c0fe97f894531f7d5adf8e1ed10ea61f091733
Abhijeet Singh <asingh9829@gmail.com>
2021-07-29 01:53:47 +0530

    added auth check to some file/folder routes

--- server/routes/file.js
+++ server/routes/file.js
@@ -7,13 +7,14 @@ const {
 	renameFile,
 	deleteFile,
 } = require('../handlers/file')
+const {isStaffUser} = require('../middleware/auth')
 
-router.post('/upload', uploadFile)
+router.post('/upload', isStaffUser, uploadFile)
 router.get('/thumbnail/:id', getThumbnail)
 router.get('/info/:id')
 router.get('/stream-video/:id')
 router.get('/list', getList)
 router.get('/download/:id', downloadFile)
-router.patch('/rename', renameFile)
-router.delete('/:fileId', deleteFile)
+router.patch('/rename', isStaffUser, renameFile)
+router.delete('/:fileId', isStaffUser, deleteFile)
 module.exports = router
--- server/routes/folder.js
+++ server/routes/folder.js
@@ -1,14 +1,15 @@
 const {getFolderList, uploadFolder, renameFolder, deleteFolder} = require('../handlers/folder')
+const {isStaffUser} = require('../middleware/auth')
 
 const router = require('express').Router()
 
-router.post('/upload', uploadFolder)
-router.delete('/delete', deleteFolder)
+router.post('/upload', isStaffUser, uploadFolder)
+router.delete('/delete', isStaffUser, deleteFolder)
 router.delete('/remove-all')
 router.get('/info/:id')
 router.get('/list', getFolderList)
 router.get('/subfolder-list')
-router.patch('/rename', renameFolder)
+router.patch('/rename', isStaffUser, renameFolder)
 // router.delete('/')
 
 module.exports = router
